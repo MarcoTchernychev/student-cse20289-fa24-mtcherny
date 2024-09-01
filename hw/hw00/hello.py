a8979773a03eacd4be856f4ebd6104e17d4e3f96
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-09-01 16:10:42 -0400

    HW00 - moved gitinfo.txt and hello.py under HW00

---
+++ hw/hw00/hello.py
@@ -0,0 +1,8 @@
+# hello.py : Demonstration file for Python
+#
+# Run this file by executing the following command:
+# python3 hello.py
+
+NetID = 'mtcherny'
+
+print('Hello, my netID is ' + NetID + '.')
