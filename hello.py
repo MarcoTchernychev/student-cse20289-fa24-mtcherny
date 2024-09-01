7893fbab55b345685d3ec1e3a4e96ad78c3c836c
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-09-01 16:12:23 -0400

    HW00 - moved gitinfo.txt and hello.py under HW00 and deleted those files from main repo

--- hello.py
+++
@@ -1,8 +0,0 @@
-# hello.py : Demonstration file for Python
-#
-# Run this file by executing the following command:
-# python3 hello.py
-
-NetID = 'mtcherny'
-
-print('Hello, my netID is ' + NetID + '.')
