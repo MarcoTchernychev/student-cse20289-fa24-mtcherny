d0db0c9a1e70a39e6ce5e1087c769142034d8bbd
Marco Tchernychev <mtcherny@student13.cse.nd.edu>
2024-09-04 18:20:20 -0400

    HW01 - made the functions

--- hw/hw01/searchsrc.py
+++ hw/hw01/searchsrc.py
@@ -2,13 +2,9 @@
 #mtcherny@nd.edu
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("file", type=str)
-parser.add_argument("--include", "--member", "--ptr", "--simplefunc", "--simplefuncec")
-args = parser.parse_args()
 
-filename = args.file
-
+#INPUT: string called filename that's from the command line
+#OUTPUT: a list where each element is a string consisting of the contents of each line in filename 
 def readFile(filename):
     lines = []
     with open(filename, 'r') as f:
@@ -16,6 +12,60 @@ def readFile(filename):
             lines.append(line)
     return lines
 
-lines = readFile(filename)
-print(lines)
+#INPUT: a list where each element is a string consisiting of each line in a file
+#OUTPUT: the number of times an include statement is detected
+def countInclude(lines):
+    count = 0
+    for line in lines:
+        if "#include" in line:
+            count+=1
+    return count
 
+#INPUT: a list where each element is a string consisiting of each line in a file
+#OUTPUT: the number of times "::" is detected
+def countMember(lines):
+    count = 0
+    for line in lines:
+        if "::" in line:
+            count+=1
+    return count
+
+#INPUT: a list where each element is a string consisiting of each line in a file
+#OUTPUT: the number of times "->" is detected
+def countPter(lines):
+    count = 0
+    for line in lines:
+        if "->" in line:
+            count+=1
+    return count
+
+#INPUT: a list where each element is a string consisiting of each line in a file
+#OUTPUT: the number of times a function is called (with the first curly brace on its own line, and the last curly brace on a seperate line) that only has a single line of code or less
+def countSimpleFunc(lines):
+    count = 0
+    for i in range(len(lines)):
+        if "::" in lines[i] and "}" in lines[i+3]:
+            count+=1
+    return count
+
+#INPUT: a list where each element is a string consisting of each line in a file
+#OUTPUT: the number of times a function is called (with the first curly brace on its own line, or on the same line as the function name, and the last curly brace on a seperate line) that only has a single line of code or less
+def countSimpleFuncEC(lines):
+    count=0
+    for i in range(len(lines)):
+        if "::" in lines[i]:
+            if ("{" in lines[i] and "}" in lines[i+2]) or ("{" in lines[i+3]):
+                count+=1
+    return count
+
+#Start of main
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=str)
+parser.add_argument("--include")
+parser.add_argument("--member")
+parser.add_argument("--ptr")
+parser.add_argument("--simplefunc")
+parser.add_argument("--simplefuncec")
+args = parser.parse_args()
+filename = args.file
+lines = readFile(filename)
