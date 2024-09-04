45e7e626d6485ddf9916561d0a40d99185ad549d
Marco Tchernychev <mtcherny@student13.cse.nd.edu>
2024-09-04 16:53:45 -0400

    HW01 - Able to load in files line by line

--- hw/hw01/searchsrc.py
+++ hw/hw01/searchsrc.py
@@ -1,4 +1,21 @@
 #Marco Tchernychev
 #mtcherny@nd.edu
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=str)
+parser.add_argument("--include", "--member", "--ptr", "--simplefunc", "--simplefuncec")
+args = parser.parse_args()
+
+filename = args.file
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line)
+    return lines
+
+lines = readFile(filename)
+print(lines)
 
