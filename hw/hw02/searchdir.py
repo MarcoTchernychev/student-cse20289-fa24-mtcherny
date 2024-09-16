f83cbab111e172cbb4025dea08d19d19c4ec1bae
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-09-15 20:06:34 -0400

    HW02 - finished task 6

--- hw/hw02/searchdir.py
+++ hw/hw02/searchdir.py
@@ -3,6 +3,8 @@
 import os
 import subprocess
 import argparse
+import csv
+import statistics
 
 #INPUT: file name and path
 #OUTPUT: data from the specified file name and path in a dictionary {lines: ..., include: ..., etc}
@@ -27,20 +29,68 @@ def print_dict(data):
 #OUTPUT: list of dicts with each file's data
 def ScanDir(path, quiet = False, recursive = False):
     dict_list = [] #list to be popoulated with data from each dict
-    for file in os.listdir(path):
-        data_dict = get_data(path, file)
-        dict_list.append(data_dict)
-        if(quiet==False):
-            print_dict(data_dict)
-    return dict_list
+    if recursive:
+        for dirpath, _, filenames in os.walk(path):
+            for file in filenames:
+                data_dict = get_data(dirpath, file)
+                dict_list.append(data_dict)
+                if(quiet==False):
+                    print_dict(data_dict)
+        return dict_list 
+    else:
+        for file in os.listdir(path):
+            data_dict = get_data(path, file)
+            dict_list.append(data_dict)
+            if(quiet==False):
+                print_dict(data_dict)
+        return dict_list
+
+#INPUT: list of dicts
+#PURPOSE: makes list of dicts into a csv file with the keys as the headers
+#OUTPUT: a csv file to the specified file name in comand line arg
+def ExtractCSV(csvName, dict_list):
+    with open(csvName, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=dict_list[0].keys()) #creating the objects making field names (headers) the keys
+        writer.writeheader() #write the keys as the headers
+        writer.writerows(dict_list) #write the rows (data)
+    return csvName
+
+#INPUT: list of dicts
+#PURPOSE: makes a table where each field is a characteristic of a file (lines, include, etc) and each header is a statistic
+#OUTPUT: a table that can be written to a .txt file called stats.txt
+def getStats(dict_list):
+    lines_list = []
+    inc_list = []
+    li_list = []
+    mf_list = []
+    olf_list = []
+    name_list = []
+    for dict in dict_list:
+        lines_list.append(int(dict['lines']))
+        inc_list.append(int(dict['include']))
+        li_list.append(int(dict['includelocal']))
+        mf_list.append(int(dict['memberfuncs']))
+        olf_list.append(int(dict['onelinefuncs']))
+        name_list.append(dict['file'])
+    with open('stats.txt', 'w') as file:
+        file.write("Field, Min, MinFile, Max, MaxFile, Mean, Median, StdDev \n")
+        file.write(f"lines: {min(lines_list)}, {name_list[lines_list.index(min(lines_list))]}, {max(lines_list)}, {name_list[lines_list.index(max(lines_list))]}, {statistics.mean(lines_list)}, {statistics.median(lines_list)}, {statistics.stdev(lines_list):.1f}\n")
+        file.write(f"includes: {min(inc_list)}, {name_list[inc_list.index(min(inc_list))]}, {max(inc_list)}, {name_list[inc_list.index(max(inc_list))]}, {statistics.mean(inc_list)}, {statistics.median(inc_list)}, {statistics.stdev(inc_list):.1f}\n")
+        file.write(f"localincludes: {min(li_list)}, {name_list[li_list.index(min(li_list))]}, {max(li_list)}, {name_list[li_list.index(max(li_list))]}, {statistics.mean(li_list)}. {statistics.median(li_list)}, {statistics.stdev(li_list):.1f}\n")
+        file.write(f"memberfuncs: {min(mf_list)}, {name_list[mf_list.index(min(mf_list))]}, {max(mf_list)}, {name_list[mf_list.index(max(mf_list))]}, {statistics.mean(mf_list)}, {statistics.median(mf_list)}, {statistics.stdev(mf_list):.1f}\n")
+        file.write(f"onelinefuncs: {min(olf_list)}, {name_list[olf_list.index(min(olf_list))]}, {max(olf_list)}, {name_list[olf_list.index(max(olf_list))]}, {statistics.mean(olf_list)}, {statistics.median(olf_list)}, {statistics.stdev(olf_list):.1f}")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", type=str)
+parser.add_argument("--file", type=str)
 parser.add_argument("path", type=str)
 parser.add_argument("-r", action="store_true", default = False, help = "denotes if directories should be processed recursively (default is false)")
-parser.add_argument("--csv XXX", action="store_true", default = None, help = "an argument that also has a filename specified for the output (default is none)")
+parser.add_argument("--csv", type=str, default = None, help = "an argument that also has a filename specified for the output (default is none)")
 parser.add_argument("--stats", action="store_true", default = False, help = "denotes if stats should be computed across the numeric fields and reported (default is false)")
 parser.add_argument("--quiet", action="store_true", default = False, help = "requests the output to stay quiet (no output of the pretty, compact one-line per file from Task 2) (default is False)")
 args = parser.parse_args()
 
-ScanDir("../hw01/src", args.quiet, args.r)
+dict_list = ScanDir("../hw01/src", args.quiet, args.r)
+ExtractCSV(args.csv,dict_list)
+
+if(args.stats):
+    getStats(dict_list)
