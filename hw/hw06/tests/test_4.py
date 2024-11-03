2cbfcf234c379d13c08f3451b58ed0fe87d23795
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-11-02 20:34:20 -0400

    HW06 - modifications to test_4.py

---
+++ hw/hw06/tests/test_4.py
@@ -0,0 +1,22 @@
+import unittest
+import hw6searchdir
+path = "/escnfs/home/mtcherny/repos/student-cse20289-fa24-mtcherny/hw/hw06/tests/data/"
+class TestClass(unittest.TestCase):
+    
+    def test_nrd1(self):
+        self.assertEqual()
+    def test_nrd2(self):
+        self.assertEqual()
+    def test_nrd3(self):
+        self.assertEqual()
+    def test_rd1(self):
+        self.assertEqual()
+    def test_rd2(self):
+        self.assertEqual()
+    def test_rd3(self):
+        self.assertEqual()
+    def test_rd4(self):
+        self.assertEqual()
+
+if __name__ == "__main__":
+    unittest.main()
